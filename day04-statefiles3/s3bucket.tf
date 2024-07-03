8181967a05fda585353ac4113a442d6930836b93
mahendhar <mahendhar@gmail.com>
2024-07-03 09:44:41 +0530

    update terraform files

--- day04-statefiles3/s3bucket.tf
+++ day04-statefiles3/s3bucket.tf
@@ -1,3 +1,15 @@
 resource "aws_s3_bucket" "cust" {
     bucket = "mahendharemmadi514"
 }
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
