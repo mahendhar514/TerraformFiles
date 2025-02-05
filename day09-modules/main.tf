87e7886c6d1d7492e4be0b15f3a3dd710bb6ce5c
mahendhar <mahendhar@gmail.com>
2025-02-05 16:14:45 +0530

    updated key

--- day09-modules/main.tf
+++ day09-modules/main.tf
@@ -2,7 +2,7 @@ module "ec2-instance" {
     source = "../day01-tfvars"
     ami = "ami-01376101673c89611"
     instance_type = "t2.micro"
-    key_name = "pemkey"
+    key_name = "AWS"
     tags = {
      Name = "modules"
     }
