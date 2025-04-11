13f6678bf09f49d9b7d086bb31abc4a07ee2233a
mahendhar <mahendhar@gmail.com>
2025-04-11 16:31:23 +0530

    update

--- day01/main.tf
+++ day01/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "localname" {
-    ami = "ami-04629cfb3bd2d73f3"
+    ami = "ami-080b1a55a0ad28c02"
     instance_type = "t2.micro"
-    key_name = "pemkey"
-    subnet_id = "subnet-0cd7d5b669714f5e6"
-    security_groups = [ "sg-05f352f5ac337804a" ]
+    count = 10
+    key_name = "AWS"
+    subnet_id = "subnet-063ae2d7ba8f201ae"
+    security_groups = [ "sg-0e858433fd4b326e7" ]
     tags = {
       Name = "terraform22"
     }
